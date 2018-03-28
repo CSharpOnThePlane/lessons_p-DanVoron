1e80c912950a9a380257225bdbf5653e548c31eb
Daniil Voronin <danvoron3@gmail.com>
2018-03-28 18:23:07 +0300

    Урок
    Просто урок.

---
+++ 24. arrays/дз1/дз1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace дз1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] masivchick = { 1, 1, 1, 5, 1, 1, 1, 1, 5 };
+            int p = masivchick.Length;
+            
+            for (int geg = 0; geg < p; geg++)
+            { 
+              if (masivchick[geg]==5)
+              {
+                Console.WriteLine("тут есть 5" + " " + "и на " + geg + "Месте");
+                break;
+              }
+            }
+        }
+    }
+}
+    
---
+++ 24. arrays/дз2/дз2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace дз2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int d = 0;
+            int[] edinairossia = { 2, 3,5, 1 ,1};
+            int h = edinairossia.Length;
+            for (int i = 0; i<h;i++)
+            {
+             
+                
+                if (edinairossia[i] < 3)
+                {
+                    d++;
+                }
+
+            }
+            Console.WriteLine(d + " " + "команда имеет меньше 3 побед");
+        }
+    }
+}
+ 

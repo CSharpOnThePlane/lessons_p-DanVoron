92ccf1efb7ee1ed35e79c9ecc46519db19314418
Daniil Voronin <36923175+DanVoron@users.noreply.github.com>
2018-04-11 18:22:42 +0300

    FIRST GAME

---
+++ ТЕСТ 0.1/Assets/top scrips ever/GoodTest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodTest : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float x = transform.position.x;
+		float y = transform.position.y;
+		float z = transform.position.z;
+		print ("x =" + x + "y =  " + y + "z = " + z );
+	}
+}
---
+++ ТЕСТ 0.1/Assets/top scrips ever/TimerForPro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerForPro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	float sec = 0;
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		float kadr = Time.deltaTime;
+		sec =  sec + kadr;
+		print ("прошло" + " " + (int)sec + " секунд");
+		//привет не воруй код		
+	}
+}

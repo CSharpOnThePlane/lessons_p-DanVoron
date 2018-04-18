fc2fb2f2644f0189b7293d06ac991894125474b4
Daniil Voronin <36923175+DanVoron@users.noreply.github.com>
2018-04-18 18:13:37 +0300

    Lesson on Unity 2
    Dz and more

---
+++ LessonOnUnity2(2)/Assets/ScrptOtLydei/RasstoanieDoKuba.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RasstoanieDoKuba : MonoBehaviour {
+	public Transform cube;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance(cube.position, transform.position);
+		print (dist);
+	}
+}
---
+++ урок 2/Assets/Every15Sec.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  Every15Sec: MonoBehaviour {
+	float sec = 0;
+	float volna = 0;
+	float sec2 = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float kadr = Time.deltaTime;
+		sec2 = sec2 + kadr;
+		sec =  sec + kadr;
+		if((int)sec%5==0)
+		{
+			print("Монстры идут");
+		}
+		if(sec2>5)
+		{
+			volna++;
+			print (volna + "идет");
+			sec2 = 0;
+		}
+	}
+}

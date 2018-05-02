4b8e5520a8d5a5b457f70454583cff4087781b1e
Daniil Voronin <36923175+DanVoron@users.noreply.github.com>
2018-05-02 18:07:14 +0300

    SuperTimerAndTeleporter

---
+++ LessonOnUnity2(2)/Assets/Scrips/Teleporter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+	public Transform cube;
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dist = Vector3.Distance(cube.position, transform.position);
+		if (dist < 10) {
+			transform.position = new Vector3 (262, 5.5f, 305);
+		}
+	}
+}
